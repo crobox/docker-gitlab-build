1a6ea02b7a5da6c53fa4106786d2c645544900c5
Nicu Reut <nicu@crobox.com>
2018-02-14 11:32:10 +0100

    Fixed deployment script

--- scripts/marathon_deploy.sh
+++ scripts/marathon_deploy.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-marathon_uri=$1
+MARATHON_FILE=
+APP_ID=
 if [ $# -eq 2 ]
   then
-    marathon_file=$2
-    app_id=$(cat marathon_file | jq -r '.id')
+    MARATHON_FILE=$2
+    APP_ID=$(cat ${MARATHON_FILE} | jq -r '.id')
 elif [ $# -eq 3 ]
   then
-    app_id=$2
-    marathon_file=$3
+    APP_ID=$2
+    MARATHON_FILE=$3
   else
     echo "Don't know how to work with this arguments."
     exit -1
 fi
+MARATHON_URI=$1
+
 max_deploy_time=${MAX_DEPLOYMENT_TIME-180}
-echo "Running deployment for $appid with file $marathon_file. Max time $max_deploy_time"
-deployment_id=$(http --check-status PUT "${marathon_uri}/v2/apps/${app_id}" < "${marathon_file}" | jq -r '.deploymentId')
+echo "Running deployment for $APP_ID with file $MARATHON_FILE. Max time $max_deploy_time"
+deployment_id=$(http --check-status PUT "${MARATHON_URI}/v2/apps/${APP_ID}" < "${MARATHON_FILE}" | jq -r '.deploymentId')
 deployment_in_progress="alive"
 echo "Created deployment with id $deployment_id"
 current_deployment_time=0
@@ -24,7 +27,7 @@ while [[  ${deployment_in_progress} != "" && ${current_deployment_time} -lt ${ma
     echo "Deployment running, waiting before retrying to check status."
     sleep 5
     current_deployment_time=$((current_deployment_time + 5))
-    deployment_in_progress=$(http --check-status GET "${marathon_uri}/v2/deployments" | jq -c '.[] | select(.id | contains($deployment_id))?' --arg deployment_id "${deployment_id}")
+    deployment_in_progress=$(http --check-status GET "${MARATHON_URI}/v2/deployments" | jq -c '.[] | select(.id | contains($deployment_id))?' --arg deployment_id "${deployment_id}")
 done
 if [[ ${current_deployment_time} -ge ${max_deploy_time} ]];then
     echo "Deployment exceeded maximum time of $max_deploy_time"
