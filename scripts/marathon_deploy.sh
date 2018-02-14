1414122b10352490ed0d3c09da6360bc5eccabba
Nicu Reut <reutnicu@gmail.com>
2018-02-14 10:41:43 +0100

    Update marathon_deploy.sh

--- scripts/marathon_deploy.sh
+++ scripts/marathon_deploy.sh
@@ -2,11 +2,21 @@
 set -Eeuo pipefail
 
 marathon_uri=$1
-appid=$2
-marathon_file=$3
+if [ $# -eq 2 ]
+  then
+    marathon_file=$2
+    app_id=$(cat marathon_file | jq -r '.id')
+elif [ $# -eq 3 ]
+  then
+    app_id=$2
+    marathon_file=$3
+  else
+    echo "Don't know how to work with this arguments."
+    exit -1
+fi
 max_deploy_time=${MAX_DEPLOYMENT_TIME-180}
 echo "Running deployment for $appid with file $marathon_file. Max time $max_deploy_time"
-deployment_id=$(http --check-status PUT "${marathon_uri}/v2/apps/${appid}" < "${marathon_file}" | jq -r '.deploymentId')
+deployment_id=$(http --check-status PUT "${marathon_uri}/v2/apps/${app_id}" < "${marathon_file}" | jq -r '.deploymentId')
 deployment_in_progress="alive"
 echo "Created deployment with id $deployment_id"
 current_deployment_time=0
